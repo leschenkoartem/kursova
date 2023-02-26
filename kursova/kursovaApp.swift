d76dc81725b47f08a1ba8bb066d88caa135f10f9
Artem <leschenko.artem@lll.kpi.ua>
2023-02-26 13:16:12 +0200

    Initial Commit

---
+++ kursova/kursovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  kursovaApp.swift
+//  kursova
+//
+//  Created by Artem Leschenko on 26.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct kursovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
